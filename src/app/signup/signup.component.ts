9bd7b4118635a88365d7bed13e207d82debbd33c
Mounir Bkr <miirbri1@gmail.com>
2024-07-19 15:00:03 +0100

    sign-up form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+import {AuthService} from "../services/auth/auth.service";
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  signupForm: FormGroup;
+  hidePassword: boolean;
+
+  constructor(private fb: FormBuilder,
+              private  snackBar: MatSnackBar,
+              private authService: AuthService, //imported from service
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]],
+
+    })
+  }
+
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit() {
+    const password = this.signupForm.get('password')?.value;
+    const confirmPasssword = this.signupForm.get('confirmPassword')?.value;
+
+    if (password !== confirmPasssword) {
+      this.snackBar.open('Password do not mutch', 'Close', {duration: 5000, panelClass: 'error-snackbar'});
+      return;
+    }
+
+     this.authService.register(this.signupForm.value).subscribe(
+      (response:any) => {
+        this.snackBar.open('Sign up successfull', 'Close', {duration: 5000, panelClass: 'error-snackbar'});
+        this.router.navigateByUrl("/login");
+      },
+      (error:any) => {
+        this.snackBar.open('Sign up failed, please try again', 'Close', {duration: 5000, panelClass: 'error-snackbar'});
+      }
+     )
+  }
 
 }
